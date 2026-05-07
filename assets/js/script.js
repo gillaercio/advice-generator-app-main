0914670a79846313eb03b4198035ee7cbb4f4a7f
Gildman Laércio <laerciokarlagabm@gmail.com>
2026-05-07 05:12:33 -0300

    fix: correct advice button position, refactor: improved behavior of the advice button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,27 @@ const adviceId = document.getElementById("advice-id");
 const adviceText = document.getElementById("advice-text");
 const button = document.querySelector(".advice__button");
 
-async function getAdvice() {
-  adviceId.textContent = "...";
-  adviceText.textContent = "Loading advice...";
-  adviceText.style.opacity = "0";
-
+async function getAdvice() {  
+  button.disabled = true;
+  
   try {
     const response = await fetch(
       `https://api.adviceslip.com/advice?timestamp=${Date.now()}`
     );
     const data = await response.json();
+    adviceText.style.opacity = "0";
 
     setTimeout(() => {
       adviceId.textContent = data.slip.id;
       adviceText.textContent = data.slip.advice;
       adviceText.style.opacity = "1";
+
+      button.disabled = false;
     }, 300);
 
-  } catch (error) {
+  } catch {
     adviceText.textContent = "Failed to load advice.";
+    adviceText.style.opacity = "1";
   }
 }
 
